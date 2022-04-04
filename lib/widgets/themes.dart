61541cc2c627111cb575e6feabf78afbec5cbff6
girijaarani9 <https://github.com/girijaarani9>
2022-04-04 13:30:12 +0530

    Day-11 updated

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      iconTheme: IconThemeData(color: Colors.black),
-      appBarTheme: AppBarTheme(color: Colors.black, elevation: 0.0),
-      textTheme: Theme.of(context).textTheme);
-
+      appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(color: Colors.black),
+          titleTextStyle: Theme.of(context).textTheme.bodyText1));
   static ThemeData DarkTheme(BuildContext context) =>
       ThemeData(brightness: Brightness.dark);
 }
