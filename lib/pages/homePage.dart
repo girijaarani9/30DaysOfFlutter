893c369cf9f32277346e9ee975b823436c21b676
girijaarani9 <https://github.com/girijaarani9>
2022-04-06 13:12:57 +0530

    Day-13 Completed

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,11 +1,32 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_days/models/catlog.dart';
 import 'package:flutter_days/widgets/item_Widget.dart';
 
 import '../widgets/drawer.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catlogJson = await rootBundle.loadString("assets/files/products.json");
+    var decodedData = jsonDecode(catlogJson);
+    var productsData = decodedData["products"];
+  }
 
   @override
   Widget build(BuildContext context) {
