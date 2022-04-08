5d4e187efe20f52b7423071f4b9843450cb4d952
girijaarani9 <https://github.com/girijaarani9>
2022-04-08 12:34:17 +0530

    Day-16 Completed

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -40,11 +40,33 @@ class _HomePageState extends State<HomePage> {
         title: Text('Catlog App'),
       ),
       body: (CatlogModel.items != null && CatlogModel.items.isNotEmpty)
-          ? ListView.builder(
+          ? GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, mainAxisSpacing: 16, crossAxisSpacing: 16),
               itemCount: CatlogModel.items.length,
               itemBuilder: (BuildContext context, int index) {
-                return ItemWidget(
-                  item: CatlogModel.items[index],
+                final item = CatlogModel.items[index];
+                return Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: GridTile(
+                    child: Image.network(item.imageUrl),
+                    header: Container(
+                      padding: EdgeInsets.all(10),
+                      color: Colors.deepPurple,
+                      child: Text(
+                        item.name,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    footer: Container(
+                        padding: EdgeInsets.all(10),
+                        color: Colors.black,
+                        child: Text(
+                          item.price.toString(),
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ),
                 );
               },
             )
